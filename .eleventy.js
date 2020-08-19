3c15772acf203c742a9686720b0f7d53e49e97ba
Anders Bo Rasmussen <anras573@gmail.com>
2020-08-19 15:26:17 +0200

    Setup 11ty and create printable weekly planner.

---
+++ .eleventy.js
@@ -0,0 +1,24 @@
+const CleanCSS = require("clean-css");
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addFilter("cssmin", function(code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
+
+  eleventyConfig.addPassthroughCopy("static");
+
+  return {
+    templateFormats: ["njk"],
+
+    pathPrefix: "/",
+
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    dir: {
+      input: ".",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
+    }
+  };
+};
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # printable-weekly-planner
-A printable weekly planner made with 11ty
+
+A printable weekly planner made with 11ty.
+
+## Requirements
+
+LTS version of [Node.js](https://nodejs.org/en/).
+
+## Setup
+
+1. Run `npm ci` in a console of your choice
+2. Run `npm start`
+3. Open [http://localhost:8080/](http://localhost:8080/) in a browser of your choice
+4. (optional) Edit the data in _data/weekplan.json
+
+## Example
+
+![Example of a printable weekly planner](static/example.png)
